351b7740fdabd196d935aec2ae923a9eb329dd66
Bob Fanger <bfanger@gmail.com>
2016-02-16 11:41:03 +0100

    Changed layout to flexbox

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -4,18 +4,24 @@
  */
 namespace Sledgehammer;
 ?>
-<div class="pageLayout">
-	<header class="titlebar">
-		<a class="titlebar-title" href="<?php echo WEBPATH; ?>">DevUtils</a>
-	</header>
-	<nav class="menu">
-		<?php render($navigation); ?>
-	</nav>
-	<?php render($breadcrumbs); ?>
-	<section class="contents clearfix">
-		<?php render($contents); ?>
-	</section>
-	<div class="statusbar-placeholder"></div>
+<div class="page">
+    <div class="page-column">
+        <header class="titlebar">
+            <a class="titlebar-title" href="<?php echo WEBPATH; ?>">DevUtils</a>
+        </header>
+        <div class="page-body">
+            <nav class="page-menu">
+                <?php render($navigation); ?>
+            </nav>
+            <div class="page-content">
+                <?php render($breadcrumbs); ?>
+                <section class="contents">
+                    <?php render($contents); ?>
+                </section>
+                <div class="statusbar-placeholder"></div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 javascript_once(WEBROOT.'core/js/jquery.js', 'jquery');
